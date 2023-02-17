90d28dc0bdfb9415b0196407200eaedd986b93f5
Bas Vegt <basvegt+github@gmail.com>
2023-02-17 13:20:47 +0100

    Created first script

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+namespace TEMPLATENAMESPACE 
+{ 
+    /// <summary>
+    /// <para>SUMMARY GOES HERE.</para>
+    /// </summary>
+    public class CameraBehaviour : MonoBehaviour
+    {
+        ///___VARIABLES___///
+        #region INSPECTOR
+        
+        #endregion
+        #region BACKEND_VARIABLES
+        
+        #endregion
+        ///___METHODS___///
+        #region LIFESPAN
+        private void Awake()
+        {
+            
+        }
+        private void Start()
+        {
+            
+        }
+        #endregion
+        #region LOOP
+        private void Update()
+        {
+            
+        }
+        #endregion
+        #region OTHER_METHODS
+        
+        #endregion
+    }
+}

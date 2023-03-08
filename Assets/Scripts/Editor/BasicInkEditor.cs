7ebef84c9c5a498a83b780d5df42b8cd66d73053
Bas Vegt <basvegt+github@gmail.com>
2023-03-08 14:58:11 +0100

    taken scripts from ink demo

---
+++ Assets/Scripts/Editor/BasicInkEditor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Ink.UnityIntegration;
+using Ink.Runtime;
+
+namespace Core
+{
+    /// <summary>
+    /// An editor custom made to view the basic ink script in unity's editors. 
+    /// Taken from the ink demo at 2023-03-08, 14:50.
+    /// </summary>
+    [CustomEditor(typeof(BasicInkScript))]
+    [InitializeOnLoad]
+    public class BasicInkEditor : Editor
+    {
+
+        static BasicInkEditor()
+        {
+            BasicInkScript.OnCreateStory += OnCreateStory;
+        }
+
+        static void OnCreateStory(Story story)
+        {
+            /// If you'd like NOT to automatically show the window and attach (your teammates may appreciate it!) then replace "true" with "false" here. 
+            InkPlayerWindow window = InkPlayerWindow.GetWindow(true);
+            if (window != null) InkPlayerWindow.Attach(story);
+        }
+        public override void OnInspectorGUI()
+        {
+            Repaint();
+            base.OnInspectorGUI();
+            var realTarget = target as BasicInkScript;
+            var story = realTarget.story;
+            InkPlayerWindow.DrawStoryPropertyField(story, new GUIContent("Story"));
+        }
+    }
+}
+
---
+++ Assets/Scripts/QuitGameOnKeypress.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Controls
+{ 
+  /// <summary>
+  /// Simply provides functionality to close the deployed game executable. 
+  /// </summary>
+  /// Taken from: Ink demo, 2023-03-08, 14:39
+	public class QuitGameOnKeypress : MonoBehaviour
+	{
+
+		public KeyCode key = KeyCode.Escape;
+
+		void Update()
+		{
+			if (Input.GetKeyDown(key)) Application.Quit();
+		}
+	}
+}
+

05d46a07783699eb7e28b5cc28701405d51c85d2
Bas Vegt <basvegt+github@gmail.com>
2023-07-22 15:15:53 +0200

    Made fading bg transitions.

--- Assets/Project/Code/Behaviour/InkParser.cs
+++ Assets/Project/Code/Behaviour/InkParser.cs
@@ -52,7 +52,7 @@ namespace ForgottenTrails
         public LayoutElement spacer;
         */
         [BoxGroup("Scene References")]
-        public Image bgImage;
+        public BackGround bgImage;
         [BoxGroup("Scene References")]
         public HorizontalLayoutGroup portraits;
         [BoxGroup("Scene References"), Required]
@@ -336,7 +336,8 @@ namespace ForgottenTrails
             story.BindExternalFunction("Spd", (float mod) => Functions.DoFunction(() => Spd(mod / 100)));
             story.BindExternalFunction("Clear", () => Functions.DoFunction(() => textProducer.ClearPage()));
             story.BindExternalFunction("Halt", (float dur) => Functions.DoFunction(() => StartCoroutine(HaltText(dur))));
-            story.BindExternalFunction("Bg", (string fileName, float dur) => Functions.DoFunction(() => SetBackdrop(fileName)));
+            story.BindExternalFunction("Bg", (string fileName, float dur) => Functions.DoFunction(() => SetBackdrop(fileName,dur)));
+            story.BindExternalFunction("FadeTo", (string color, float dur) => Functions.DoFunction(() => SetColor(color, dur)));
             story.BindExternalFunction("Sprites", (string fileNames) => Functions.DoFunction(() => SetSprites(fileNames)));
             story.BindExternalFunction("Vox", (string fileName, float relVol) => Functions.DoFunction(() => ParseAudio(fileName,AudioManager.AudioGroup.Voice, relVol)));
             story.BindExternalFunction("Sfx", (string fileName, float relVol) => Functions.DoFunction(() => ParseAudio(fileName, AudioManager.AudioGroup.Sfx, relVol)));
@@ -388,15 +389,14 @@ namespace ForgottenTrails
             Halted = false;
         }
 
-        private void SetBackdrop(string fileName)
+        private void SetBackdrop(string fileName, float duration = 0)
         {
-            if (peeking) return;
             Sprite sprite = null; /// clear bg if no other value is given
             if (!(fileName == "" | fileName == "null"))
             {
                 try
                 {
-                    if (!AssetManager.Instance.Sprites.TryGetValue(fileName, out Sprite sprite1))
+                    if (!AssetManager.Instance.Sprites.TryGetValue(fileName.ToLower(), out Sprite sprite1))
                     {
                         throw new FileNotFoundException("File not found: " + fileName);
                     }
@@ -411,11 +411,23 @@ namespace ForgottenTrails
                     // throw it to the parent method.
                     if (e.Source != null)
                         Console.WriteLine("IOException source: {0}", e.Source);
-                    throw;
+                    Debug.LogException(e);
                 }
             }
-            bgImage.sprite = sprite;
             inkData.sceneState.background = fileName;
+            if (duration > 0)
+            {
+                StartCoroutine(bgImage.FadeTo(sprite,duration));
+            }
+            else
+            {
+                bgImage.SnapTo(sprite);
+            }
+        }
+
+        private void SetColor(string color, float duration = 0)
+        {
+            StartCoroutine(bgImage.FadeTo(color, duration));
         }
 
         private void SetSprites(string fileNames)
@@ -438,7 +450,7 @@ namespace ForgottenTrails
                 {
                     try
                     {
-                        if (!AssetManager.Instance.Sprites.TryGetValue(fileName, out Sprite sprite1))
+                        if (!AssetManager.Instance.Sprites.TryGetValue(fileName.ToLower(), out Sprite sprite1))
                         {
                             Debug.LogError(new FileNotFoundException("File not found: " + fileName));
                         }
@@ -453,7 +465,7 @@ namespace ForgottenTrails
                         // throw it to the parent method.
                         if (e.Source != null)
                             Console.WriteLine("IOException source: {0}", e.Source);
-                        throw;
+                        Debug.LogException(e);
                     }
                 }
 
@@ -471,7 +483,7 @@ namespace ForgottenTrails
             {
                 try
                 {
-                    if (!AssetManager.Instance.AudioClips.TryGetValue(fileName, out AudioClip audioClip1))
+                    if (!AssetManager.Instance.AudioClips.TryGetValue(fileName.ToLower(), out AudioClip audioClip1))
                     {
                         throw new FileNotFoundException("File not found: " + fileName);
                     }
@@ -486,7 +498,7 @@ namespace ForgottenTrails
                     // throw it to the parent method.
                     if (e.Source != null)
                         Console.WriteLine("IOException source: {0}", e.Source);
-                    throw;
+                    Debug.LogException(e);
                 }
             }
             if (relVol > 1)
@@ -698,9 +710,9 @@ namespace ForgottenTrails
                         
 
                     }
-                    catch (Exception)
+                    catch (Exception e)
                     {
-                        throw;
+                        throw e;
                     }
                     Debug.Log("Successfully loaded data!");
                     return true;

a428cce1fd97725839f0eb8b017d48ea70ea1517
Bas Vegt <basvegt@gmail.com>
2023-12-19 19:38:48 +0100

    No idea tbh
    new files from new device

---
+++ Assets/_Project/Scripts/C#/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+// ------------------------------------------------------------------------------
+// NewBehaviourScript
+// Created on: #CREATIONDATE#
+// Author: #DEVELOPER#
+// Purpose: #PURPOSE#
+// ------------------------------------------------------------------------------
+
+using UnityEngine;
+
+namespace #PROJECTNAME#
+{
+    public class NewBehaviourScript : MonoBehaviour
+    {
+        #region Variables //Fields & Properties
+            
+        #endregion
+
+        #region Unity Callbacks
+            
+        #endregion
+
+        #region Custom Methods
+            
+        #endregion
+    }
+}

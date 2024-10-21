ac541c6746db5728f63a01172e29620affe2a891
Bas Vegt <basvegt+github@gmail.com>
2024-10-21 16:40:45 +0200

    improved signals and connections
    Working on changes being applied, however for some reason it's not really taking yet

--- ForgottenTrails_Godot/settings/setting_container.gd
+++ ForgottenTrails_Godot/settings/setting_container.gd
@@ -5,48 +5,80 @@ extends Control
 
 @onready var input:Control = get_node("Value Input")
 
+var values:
+	get:
+		return Settings.setting_items[setting_reference]
 var default_value:
 	get:
 		return Settings.setting_items[setting_reference].default_value
 var saved_value:
 	get:
 		return Settings.setting_items[setting_reference].saved_value
-var slider_value:
+	set(value):
+		Settings.setting_items[setting_reference].saved_value = value
+
+var input_value:
 	get:
-		return input.value
+		if input is Range:
+			return input.value
+		elif input is OptionButton:
+			return input.selected
+	set(value):
+		if input is Range:
+			input.value = value
+		elif input is OptionButton:
+			input.selected = value
 
 var change_pending: bool:
 	get:
-		return saved_value!=slider_value
+		print(saved_value!=input_value)
+		return saved_value!=input_value
 		#return slider.is_node_ready() && saved_value!=slider_value
 
 @export var reset_button:Button# = get_node("H/Reset")
 func reset():
-	slider_value=default_value
+	input_value=default_value
 
 @export var revert_button:Button# = get_node("H/Revert")
 func revert():
-	slider_value=saved_value
+	input_value=saved_value
 
 @export var apply_button:Button# = get_node("H/Apply")
 func apply():
-	saved_value=slider_value
-	setting_changed.emit(slider_value)
+	saved_value=input_value
+	change_applied.emit(input_value)
+	check_buttons()
 
-func _ready():
-	slider_value = saved_value
-	revert_button.hide()
-	apply_button.hide()
-
-
-
-func _on_value_changed(new_value):
-	if saved_value != slider_value:
+func check_buttons():
+	if saved_value != input_value:
 		revert_button.show()
 		apply_button.show()
 	else:
 		revert_button.hide()
 		apply_button.hide()
-		
+	if saved_value != default_value:
+		reset_button.show()
+	else:
+		reset_button.hide()
 
-signal setting_changed(slider_value) #TODO Room to add behaviour to see the new bheaviour from the changes, such as redrawing boxes with new opcaity
+func _check_input():
+	check_input(input_value)
+
+func check_input(new_value):
+	check_buttons()
+
+signal change_applied(slider_value) 
+
+func _on_change_applied():
+	#TODO Room to add behaviour to see the new bheaviour from the changes, such as redrawing boxes with new opcaity
+	pass
+
+
+func _ready():
+	if input is Range:
+		input.value = saved_value
+		input.min_value = values.min_value
+		input.max_value = values.max_value
+	elif input is OptionButton:
+		input.selected = default_value
+	check_buttons()

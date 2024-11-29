c2156025c1e3d3433a59bd244905027e23ff02c8
Bas Vegt <basvegt+github@gmail.com>
2024-11-29 17:05:33 +0100

    added sample save load functions from godot

--- ForgottenTrails_Godot/project/_globals/player_settings.gd
+++ ForgottenTrails_Godot/project/_globals/player_settings.gd
@@ -4,6 +4,9 @@ extends Node
 @onready var _opacity: setting_float=setting_float.new(.5,0,1)
 @onready var _speed: setting_float=setting_float.new(50,0,100)
 
+func _ready():
+	add_to_group("persist")
+
 enum Keys{
 	NA,
 	test,

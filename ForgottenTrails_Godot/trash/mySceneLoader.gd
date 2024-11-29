d491cda4142d4f3298250781418bb1f7a0077096
Bas Vegt <basvegt+github@gmail.com>
2024-11-29 16:16:24 +0100

    start game knop gemaatk
    nog geen verschil tussen new game of continue

--- ForgottenTrails_Godot/trash/mySceneLoader.gd
+++ ForgottenTrails_Godot/trash/mySceneLoader.gd
@@ -2,6 +2,14 @@ extends Node
 
 var scene = preload("res://project/main_gameplay_scene.tscn")
 
-func _ready():
-	var instance = scene.instantiate()
+var instance
+
+func _ready(): 
+	instance = scene.instantiate()
+
+func launch_game():
 	get_tree().change_scene_to_packed(scene)
+
+
+func _on_start_game_pressed():
+	launch_game()

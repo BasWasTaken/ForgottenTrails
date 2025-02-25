72d4a1e5e733d86e83a8db2bc7542ab865edd2d0
Bas Vegt <basvegt+github@gmail.com>
2025-02-25 21:18:22 +0100

    Created a signalbus and made it autoload

---
+++ ForgottenTrails_Godot/project/signal_bus.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# Define global signals
+# signal story_continued
+# signal story_printed
+# signal story_skipped
+# signal background_changed(path)
+# signal vn_state_changed(state)
+# signal choice_selected(index)
+# signal game_saved
+# signal game_loaded
+# signal game_reset

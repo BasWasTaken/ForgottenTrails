7a877566174f7524f7ba34dece82bffefbd4bb34
Bas Vegt <basvegt+github@gmail.com>
2025-03-01 14:42:57 +0100

    added notes

--- ForgottenTrails_Godot/project/signal_bus.gd
+++ ForgottenTrails_Godot/project/signal_bus.gd
@@ -17,3 +17,4 @@ extends Node
 # Ignore the warning about unused signals- they are used in other scripts
 signal user_skip_requested
 signal printer_text_finished
+#signal menu_open_requested(window:Control) leaving this signal for later, when|if i change how windows are management, or the current implementation starts causing problems

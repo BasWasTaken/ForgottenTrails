08c84b2393456bc2960bcbc5a7c8bfec2ed47436
Bas Vegt <basvegt+github@gmail.com>
2025-04-30 16:37:14 +0200

    made global button sounds

--- ForgottenTrails_Godot/project/audio_manager.gd
+++ ForgottenTrails_Godot/project/audio_manager.gd
@@ -25,3 +25,22 @@ func play_audio(stream: AudioStream, source: String):
 	player.stream = stream
 	# Play the audio
 	player.play()
+
+var button_click_sound: AudioStream = preload("res://project/system/button_click.mp3")
+var button_release_sound: AudioStream = preload("res://project/system/button_release.mp3")
+
+func _ready():
+	# Connect the signal to the function
+	# get the signal from godot's event system
+	SignalBus.ui_button_clicked.connect(_on_button_clicked)
+	SignalBus.ui_button_released.connect(_on_button_released)
+
+
+func _on_button_clicked():
+	# Play the button click sound
+	AudioManager.play_audio(button_click_sound, "sfx")
+	# Optionally, you can add more logic here if needed
+func _on_button_released():
+	# Play the button release sound
+	AudioManager.play_audio(button_release_sound, "sfx")
+	# Optionally, you can add more logic here if needed
--- ForgottenTrails_Godot/project/button_click_sounder.gd
+++ ForgottenTrails_Godot/project/button_click_sounder.gd
@@ -1,20 +1,3 @@
 extends Node
 
-var button_click_sound: AudioStream = preload("res://project/system/button_click.mp3")
-var button_release_sound: AudioStream = preload("res://project/system/button_release.mp3")
-
-func _ready():
-	# Connect the signal to the function
-	# get the signal from godot's event system
-	SignalBus.button_clicked.connect(_on_button_clicked)
-	SignalBus.button_released.connect(_on_button_released)
-
-
-func _on_button_clicked():
-	# Play the button click sound
-	AudioManager.play_audio(button_click_sound, "sfx")
-	# Optionally, you can add more logic here if needed
-func _on_button_released():
-	# Play the button release sound
-	AudioManager.play_audio(button_release_sound, "sfx")
-	# Optionally, you can add more logic here if needed
+# moved to audio_manager.gd

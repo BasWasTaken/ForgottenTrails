a56055773122e2e2d97bfc636f8a39fcf9eee011
Bas Vegt <basvegt+github@gmail.com>
2025-05-04 12:17:20 +0200

    applied naming convention to bg
    and code for it

--- ForgottenTrails_Godot/project/scripts/ink_facilitation/backdrop.gd
+++ ForgottenTrails_Godot/project/scripts/ink_facilitation/backdrop.gd
@@ -13,6 +13,6 @@ var img: Texture2D = Texture2D.new():
 		set_texture(value)
 
 func fade_to_image(prompt: String, duration: float=0.0):
-	var search = load("res://project/assets/images/" + prompt + ".jpg")
+	var search = load("res://project/assets/visual/backgrounds/img_bg_" + prompt + ".jpg") #allowing different images would be a matter of rewriting so that it does not always look in background folder
 	assert(search, "Image not found")
 	img = search

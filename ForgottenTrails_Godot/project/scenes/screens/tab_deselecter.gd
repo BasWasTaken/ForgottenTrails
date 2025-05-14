dfaa36d3c7d4e87b92d6b6ac9abb29b5b7fe3bfd
Bas Vegt <basvegt+github@gmail.com>
2025-05-14 11:41:37 +0200

    switch between blocking mouse and not, when enabling and disabling tabmenu

--- ForgottenTrails_Godot/project/scenes/screens/tab_deselecter.gd
+++ ForgottenTrails_Godot/project/scenes/screens/tab_deselecter.gd
@@ -2,4 +2,14 @@ extends TabContainer
 
 func _ready():
 	# Connect the signal to handle tab changes
+	tab_changed.connect(_on_visibility_changed)
+	visibility_changed.connect(_on_visibility_changed)
 	current_tab = -1
+
+func _on_visibility_changed():
+	if current_tab == -1 || is_visible_in_tree(): # if no tab is selected, do nothing
+		hide()
+		mouse_filter = Control.MOUSE_FILTER_IGNORE
+	else:
+		show()
+		mouse_filter = Control.MOUSE_FILTER_PASS

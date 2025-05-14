40dc7f9b83a4009a77adc44a5e9bbf5695146f8a
Bas Vegt <basvegt+github@gmail.com>
2025-05-14 12:50:14 +0200

    added notes for later

--- ForgottenTrails_Godot/project/scenes/screens/tab_deselecter.gd
+++ ForgottenTrails_Godot/project/scenes/screens/tab_deselecter.gd
@@ -8,8 +8,11 @@ func _ready():
 
 func _on_visibility_changed():
 	if current_tab == -1 || is_visible_in_tree(): # if no tab is selected, do nothing
+		#TODO set free the state
 		hide()
 		mouse_filter = Control.MOUSE_FILTER_IGNORE
 	else:
+		#TODO lock the ink interaction by changing the vn state here.
+		#NOTE left to do after I make some sort of GUI manager to control what's visible and active
 		show()
 		mouse_filter = Control.MOUSE_FILTER_PASS

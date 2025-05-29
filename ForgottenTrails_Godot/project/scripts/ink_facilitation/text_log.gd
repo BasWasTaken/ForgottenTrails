bc68a959de34201e2710f36a7b6fe1ef027a8970
Bas Vegt <basvegt+github@gmail.com>
2025-05-29 15:32:31 +0200

    extracted text from choices succesfully

--- ForgottenTrails_Godot/project/scripts/ink_facilitation/text_log.gd
+++ ForgottenTrails_Godot/project/scripts/ink_facilitation/text_log.gd
@@ -1,18 +1,19 @@
 extends RichTextLabel
-
+# TODO: make this a moveable window
 func _ready():
 	clear()
 	SignalBus.ink_sent_story.connect(log_story)
 	SignalBus.ink_sent_choices.connect(log_choices)
+	SignalBus.control_requests_choice.connect(log_decision)
 
 func log_story(story: String):
 	# Add the story text to the log
 	append_text(story)
 
-func log_choices(choices: Array[InkChoice]):
+func log_choices(choices: Array):
 	# Add the choices to the log
-	for choice in choices:
-		append_text("\t") 
-		append_text(choice.text)
-		append_text("\n")  # Add a newline after each choice
-	# TODO: indicate which choice was selected
+	for choice: InkChoice in choices:
+		append_text("\t" + str(choice.Index) + choice.Text + "\n")  # Add a newline after each choice
+
+func log_decision(index: int):
+	append_text("chose " + str(index))

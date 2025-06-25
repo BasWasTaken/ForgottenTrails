b357907ca8a996f7685f19ef3d33d9a6f4d865c7
Bas Vegt <basvegt+github@gmail.com>
2025-06-25 13:34:32 +0200

    hid warnings on the event bus
    this was clogging up the debugging, finalyl figured out how to remove them

--- ForgottenTrails_Godot/project/scripts/autoloads/signal_bus.gd
+++ ForgottenTrails_Godot/project/scripts/autoloads/signal_bus.gd
@@ -20,6 +20,7 @@ extends Node
 
 #signal menu_open_requested(window:Control) leaving this signal for later, when|if i change how windows are management, or the current implementation starts causing problems
 
+@warning_ignore_start("unused_signal")
 
 signal printer_text_finished
 # signal control_requests_accept

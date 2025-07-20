94896aeb1869550e892216a932dabbc3f50024b9
Bas Vegt <basvegt+github@gmail.com>
2025-07-20 10:25:50 +0200

    basic savegame selection in game works
    as in the windows is functional. have not started work on the actual loading going correctly

--- ForgottenTrails_Godot/project/scripts/save_selector.gd
+++ ForgottenTrails_Godot/project/scripts/save_selector.gd
@@ -2,6 +2,22 @@ extends OptionButton
 
 func toggle_hidden():
 	if visible:
+		_depopulate()
 		hide()
 	else:
 		show()
+		populate()
+
+func populate():
+	clear()
+	var save_slots = DataManager.get_files(DataManager.player_name,"any")
+	for save_slot in save_slots:
+		add_item(save_slot)#.get_file().get_basename())
+
+func _depopulate():
+	clear()
+
+
+func _on_confirm_load_game_pressed():
+	print("loading game from save slot: " + get_item_text(get_selected()))
+	DataManager.load_game(get_item_text(get_selected()))

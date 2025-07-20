7222368d5242daf83243ba80d9f4b58172aa4031
Bas Vegt <basvegt+github@gmail.com>
2025-07-20 10:39:29 +0200

    created cancel button for loads

--- ForgottenTrails_Godot/project/scripts/save_selector.gd
+++ ForgottenTrails_Godot/project/scripts/save_selector.gd
@@ -1,12 +1,27 @@
 extends OptionButton
 
+func choose_visible(value: bool):
+	if value:
+		show()
+	else:
+		hide()
+
 func toggle_hidden():
 	if visible:
-		_depopulate()
+		#_depopulate()
 		hide()
 	else:
 		show()
+		#populate()
+
+func _ready():
+	visibility_changed.connect(_on_visibility_changed)
+
+func _on_visibility_changed():
+	if visible:
 		populate()
+	else:
+		_depopulate()
 
 func populate():
 	clear()
@@ -21,3 +36,5 @@ func _depopulate():
 func _on_confirm_load_game_pressed():
 	print("loading game from save slot: " + get_item_text(get_selected()))
 	DataManager.load_game(get_item_text(get_selected()))
+	hide()
+	#TODO hide elements from code after a succesful load has been confirmed, not by grossly emmittiny a signal

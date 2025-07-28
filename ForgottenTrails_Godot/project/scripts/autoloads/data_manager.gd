b4177ac7bae8703d825b55489c9423a46214ac47
Bas Vegt <basvegt+github@gmail.com>
2025-07-28 17:52:07 +0200

    clear buttons before loading

--- ForgottenTrails_Godot/project/scripts/autoloads/data_manager.gd
+++ ForgottenTrails_Godot/project/scripts/autoloads/data_manager.gd
@@ -150,6 +150,11 @@ func load_game(file: String):
 	var save_file = FileAccess.open(file, FileAccess.READ)
 
 	# Clear existing objects in the "persist" group to prevent duplication
+	
+	# TODO: Manually clear text or set some sort of "you should not see this" text to the textpresenter?
+	SignalBus.request_clear_buttons.emit() # Emit signal to clear buttons, if needed
+	printer_state.set_state(printer_state.VN_State.LOCKED)
+
 	# var save_nodes = get_tree().get_nodes_in_group("persist")
 	# for node in save_nodes:
 	# 	node.queue_free()
@@ -161,7 +166,6 @@ func load_game(file: String):
 	
 	saved_state = state # Store the state in the DataManager for later use (e.g. quicksave)
 	load_story_state.emit(state) # Emit signal to load the story state
-	#TODO: Can be improved with bugfixing - currently a bit volatile after loading, clickling l keeps continueing in a sense.
 	# should really make sure that all processes are ahalted before loading, like its a still system and a clean start
 
 	while save_file.get_position() < save_file.get_length():
@@ -173,10 +177,10 @@ func load_game(file: String):
 		if parse_result != OK:
 			print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
 			continue
-
 		# Extract node data
 		var node_data = json.data
 
+		# TODO: Ensure the log item is included in this.
 		# # Recreate the object in the scene tree
 		# var new_object = load(node_data["filename"]).instantiate()
 		# get_node(node_data["parent"]).add_child(new_object)
@@ -187,4 +191,5 @@ func load_game(file: String):
 		# 	if key not in ["filename", "parent", "pos_x", "pos_y"]:
 		# 		new_object.set(key, node_data[key])
 		
+	printer_state.set_state(printer_state.VN_State.WAITING)
 	print("Game loaded from file: " + file)
